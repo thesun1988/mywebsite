723d3141affaca745cf049fc060de6a711558498
Nhat Tran <nhattran@Nhats-MacBook-Air.local>
2023-09-23 20:51:21 -0700

    add dark mode, model, project section

---
+++ javascript/myscript.js
@@ -0,0 +1,45 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    // Check for a saved dark mode preference and apply it
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      body.classList.add('dark-mode');
+      darkModeToggle.textContent = 'Light Mode'; 
+    }
+    
+
+
+    // Toggle dark mode when the button is clicked
+    darkModeToggle.addEventListener('click', () => {
+      body.classList.toggle('dark-mode');
+      // Save the dark mode preference to localStorage
+      if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+        darkModeToggle.textContent='Light Mode';
+      } else {
+        localStorage.setItem('darkMode', 'disabled');
+        darkModeToggle.textContent='Dark Mode';
+      }
+    });
+
+    //
+    const openModalBtn = document.getElementById('openModalBtn');
+    const contactModal = document.getElementById('contactModal');
+    const closeModalBtn = document.getElementById('closeModal');
+
+    // Open the modal when the button is clicked
+    openModalBtn.addEventListener('click', () => {
+      contactModal.style.display = 'block';
+    });
+
+    // Close the modal when the close button is clicked
+    closeModalBtn.addEventListener('click', () => {
+      contactModal.style.display = 'none';
+    });
+
+    // Close the modal when clicking outside the modal content
+    window.addEventListener('click', (event) => {
+      if (event.target === contactModal) {
+        contactModal.style.display = 'none';
+      }
+    });
